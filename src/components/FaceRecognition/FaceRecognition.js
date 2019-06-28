5a2491c868812fc1667a060cc3f9a3f147a3ce4d
Thomas Vu <thomasvu@Thomass-MacBook-Air.local>
2019-06-29 01:27:04 +0200

    Fixed idling img tag

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,30 +3,32 @@ import './FaceRecognition.css';
 
 const FaceRecognition = props => (
   <div className='center ma'>
-        <div className='absolute mt2'>
-          <img
-            id='inputimage'
-            width='500px'
-            height='auto'
-            src={props.imageURL}
-            alt=' '
-          />
-          {props.boxes.map(box=>(
-            <div key={box.id}
-            className='bounding-box'
-            style={{
-              top: box.topRow,
-              right: box.rightCol,
-              bottom: box.bottomRow,
-              left: box.leftCol,
-            }}
-          />
-        
-          ))}
-          </div>
-      </div>
-)
+    <div className='absolute mt2'>
+      {props.imageURL && (
+        <img
+          id='inputimage'
+          width='500px'
+          height='auto'
+          src={props.imageURL}
+          alt=' '
+        />
+      )}
 
+      {props.boxes.map(box => (
+        <div
+          key={box.id}
+          className='bounding-box'
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        />
+      ))}
+    </div>
+  </div>
+);
 
 export default FaceRecognition;
 
